c487063ac96893835a224840eb4091addd7a09b4
fbara <fbara1@yahoo.com>
2022-03-05 13:47:26 -0600

    Add custom text fields to LoginView.

---
+++ TwitterSwiftUI/Core/Components/CustomInputField.swift
@@ -0,0 +1,40 @@
+//
+//  CustomInputFields.swift
+//  TwitterSwiftUI
+//
+//  Created by Frank Bara on 3/5/22.
+//
+
+import SwiftUI
+
+struct CustomInputField: View {
+    
+    let imageName: String
+    let placeholderText: String
+    
+    @Binding var text: String
+    
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(Color(.darkGray))
+                
+                TextField(placeholderText, text: $text)
+            }
+            
+            Divider()
+                .background(Color(.darkGray))
+        }
+    }
+}
+
+struct CustomInputFields_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomInputField(imageName: "envelope", placeholderText: "Email", text: .constant(""))
+    }
+}
